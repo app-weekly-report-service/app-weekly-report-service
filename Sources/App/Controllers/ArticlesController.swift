6c57eb9fd014ef9e2132c1786dc308ff91922344
king <3925@winnermedical.com>
2022-06-22 17:29:19 +0800

    修复批量删除文章不会抛出错误的问题

--- Sources/App/Controllers/ArticlesController.swift
+++ Sources/App/Controllers/ArticlesController.swift
@@ -87,7 +87,7 @@ struct ArticlesController: RouteCollection {
     func deletes(_ req: Request) async throws -> AppResponse<Bool> {
         try DeleteArtilesContent.validate(content: req)
         let content = try req.content.decode(DeleteArtilesContent.self)
-        await withThrowingTaskGroup(of: Void.self, body: { taskGroup in
+        try await withThrowingTaskGroup(of: Void.self, body: { taskGroup in
             content.ids.forEach { id in
                 taskGroup.addTask {
                     /// 查询文章是否存在
@@ -97,6 +97,7 @@ struct ArticlesController: RouteCollection {
                     try await article.delete(on: req.db)
                 }
             }
+            try await taskGroup.waitForAll()
         })
         return .init(success: true)
     }
